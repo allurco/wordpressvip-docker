cb641805899d58d15af428333aa5aa1b48fea51c
Rogers Sampaio <r@allur.co>
2021-06-05 12:45:15 -0300

    MErge Master

--- conf/wp-local-config.php
+++ conf/wp-local-config.php
@@ -1,29 +1,36 @@
 <?php
 /**
- * WordPress local config
- *
- * @package docker-wordpress-vip-go
+ * WordPress local config.
  */
 
 // Conditionally turn on HTTPS since we're behind nginx-proxy.
-if ( isset( $_SERVER['HTTP_X_FORWARDED_PROTO'] ) && 'https' === $_SERVER['HTTP_X_FORWARDED_PROTO'] ) { // Input var ok.
-	$_SERVER['HTTPS'] = 'on';
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && 'https' === $_SERVER['HTTP_X_FORWARDED_PROTO']) { // Input var ok.
+    $_SERVER['HTTPS'] = 'on';
 }
 
 // Indicate VIP Go environment.
-define( 'VIP_GO_ENV', 'local' );
+define('VIP_GO_ENV', 'local');
 
 // Disable automatic updates.
-define( 'AUTOMATIC_UPDATER_DISABLED', true );
+define('AUTOMATIC_UPDATER_DISABLED', true);
 
 // This provides the host and port of the development Memcached server. The host
 // should match the container name in `docker-compose.memcached.yml`. If you
 // aren't using Memcached, it will simply be ignored.
-$memcached_servers = array(
-	array(
-		'memcached',
-		11211,
-	),
-);
+$memcached_servers = [
+    [
+        'memcached',
+        11211,
+    ],
+];
 
-define( 'WP_ALLOW_MULTISITE', true );
+define('WP_ALLOW_MULTISITE', true);
+define('MULTISITE', true);
+define('SUBDOMAIN_INSTALL', false);
+define('DOMAIN_CURRENT_SITE', 'xpinc-eventos.test');
+define('PATH_CURRENT_SITE', '/');
+define('SITE_ID_CURRENT_SITE', 1);
+define('BLOG_ID_CURRENT_SITE', 1);
+
+define('WP_DEBUG', true);
+define('WP_DEBUG_LOG', true);
